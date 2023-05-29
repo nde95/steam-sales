26783720f249e37c0aa7b14e2d6914da74ce64e0
nde95 <ndennison268@gmail.com>
2023-05-29 12:58:08 -0400

    initial working commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import GameCard from './components/games-card/games-card.component'
+
+function App() {
+  
+
+  return (
+    <>
+    <GameCard />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/games-card/games-card.component.jsx
@@ -0,0 +1,12 @@
+import gamesData from '../games.json';
+
+
+const GameCard = () => {
+    console.log(gamesData);
+
+    return(
+        'this is a game card'
+    )
+}
+
+export default GameCard;
