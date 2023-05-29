deb4e5aa6eb2f8c046baa7c75f75dbc2650a5ce8
nde95 <ndennison268@gmail.com>
2023-05-29 14:02:08 -0400

    mapping game data with basic temp style

--- src/components/games-card/games-card.component.jsx
+++ src/components/games-card/games-card.component.jsx
@@ -1,12 +1,25 @@
 import gamesData from '../games.json';
+import SteamButton from '../steam-button/steam-button.component';
+import './games-card.styles.css';
 
 
 const GameCard = () => {
     console.log(gamesData);
 
-    return(
-        'this is a game card'
-    )
-}
+    return (
+        <div className='game-card-container'>
+            <div className='game-card'>
+            {gamesData.games.map((game) => (
+                <div key={game.name} className='game'>
+                <h2>{game.name}</h2>
+                <img src={game.thumbnail} alt={game.name} />
+                <p>{game.description}</p>
+                <SteamButton steamPageLink={game.steamPageLink} />
+                </div>
+            ))}
+            </div>
+        </div>
+      );
+    };
 
 export default GameCard;
