658dfd9a466729f7b1230d85e6c360018473b458
nde95 <ndennison268@gmail.com>
2023-06-01 20:18:49 -0400

    conditionally mapping different schema with button

--- src/components/game-card-db/game-card-db.component.jsx
+++ src/components/game-card-db/game-card-db.component.jsx
@@ -1,58 +1,78 @@
 import { useEffect, useState } from 'react';
 import SteamButton from '../steam-button/steam-button.component';
 import './game-card-db.styles.css';
-// import ExpiredSteamButton from '../expired-steam-button/expired-steam-button.component';
-import { getGames } from '../../utils/firebase.utils';
+import { getGames, getDlc } from '../../utils/firebase.utils';
 
 const GameCard2 = () => {
   const currentDate = new Date();
-  const [games, setGames] = useState([]);
+  const [displayMode, setDisplayMode] = useState('games');
+  const [gameData, setGameData] = useState([]);
+  const [dlcData, setDlcData] = useState([]);
 
   useEffect(() => {
     // Fetch the games data
     const fetchGames = async () => {
-      const gamesData = await getGames();
-      setGames(gamesData);
+      const games = await getGames();
+      setGameData(games);
+    };
+
+    // Fetch the DLC data
+    const fetchDlc = async () => {
+      const dlc = await getDlc();
+      setDlcData(dlc);
     };
 
     fetchGames();
+    fetchDlc();
   }, []);
 
+  const handleDisplayModeToggle = () => {
+    // Toggle the display mode between games and DLC
+    const newDisplayMode = displayMode === 'games' ? 'dlc' : 'games';
+    setDisplayMode(newDisplayMode);
+  };
+
+  const activeData = displayMode === 'games' ? gameData : dlcData;
 
   return (
+    <div>
+    <div className='display-toggle'>
+      <button onClick={handleDisplayModeToggle}>
+        {displayMode === 'games' ? 'Games' : 'DLC'}
+      </button>
+    </div>
     <div className='game-card-container'>
       <div className='game-card'>
-        {games.map((game) => (
+        {activeData.map((data) => (
           <div
-            key={game.date_added}
-            className={`game ${game.is_free ? '' : 'expired'}`}
+            key={data.date_added}
+            className={`game ${data.is_free ? '' : 'expired'}`}
           >
-            <h2>{game.name}</h2>
-            <img src={game.header_image} alt={game.name} />
+            <h2>{data.name}</h2>
+            <img src={data.header_image} alt={data.name} />
             <div className='game-genres'>
-              {/* store genres as separate entries with a map */}
-              {game.genres.map((genre) => (
+              {data.genres.map((genre) => (
                 <span key={genre} className='genre-badge'>{genre}</span>
               ))}
             </div>
-            <p>{game.short_description}</p>
-            {/* switch to disabled card on date expiry  */}
-            {game.is_free ? (
-                <>
-                    <span className='sale-active'>Sale currently active</span>
-                    <SteamButton appId={game.steam_appid} />
-                </>
-                ) : (
-                <>
-                    <span className='sale-over'>Sale ended</span>
-                    <SteamButton appId={game.steam_appid} isExpired={!game.is_free} />
-                </>
-                )}
+            <p>{data.short_description}</p>
+            {data.is_free ? (
+              <>
+                <span className='sale-active'>Sale currently active</span>
+                <SteamButton appId={data.steam_appid} />
+              </>
+            ) : (
+              <>
+                <span className='sale-over'>Sale ended</span>
+                <SteamButton appId={data.steam_appid} isExpired={!data.is_free} />
+              </>
+            )}
           </div>
         ))}
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default GameCard2;
