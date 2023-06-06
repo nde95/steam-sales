7542bbdeef0ea3eb7d6c698157ab3a79b2e44d0d
nde95 <ndennison268@gmail.com>
2023-06-06 19:37:32 -0400

    routing to discord page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, Outlet } from 'react-router-dom'
 import './App.css'
 import GameCard2 from './components/game-card-db/game-card-db.component'
 import Logo from './components/logo/logo.component'
+import DiscordDocs from './components/discord-docs/discord-docs.component'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Outlet />}>
           <Route index element={<GameCard2 />} />
+          <Route path="/discord" element={<DiscordDocs />} /> 
         </Route>
       </Routes>
     </>
