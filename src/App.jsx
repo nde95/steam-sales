34befb0a85e8b8553f833fd4ebeb99057eeabd0d
nde95 <ndennison268@gmail.com>
2023-06-06 18:50:05 -0400

    reworking home page to work with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
+import { Route, Routes, Outlet } from 'react-router-dom'
 import './App.css'
 import GameCard2 from './components/game-card-db/game-card-db.component'
 import Logo from './components/logo/logo.component'
 
 function App() {
-  
-
   return (
     <>
-    <Logo />
-    <GameCard2 />
+      <Logo />
+      <Routes>
+        <Route path="/" element={<Outlet />}>
+          <Route index element={<GameCard2 />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
