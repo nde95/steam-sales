d84ca46079668b1d58f259a83b8539be80b0fff1
nde95 <ndennison268@gmail.com>
2023-06-06 21:34:44 -0400

    adding home route on logo

--- src/components/logo/logo.component.jsx
+++ src/components/logo/logo.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import logoImage from '../../assets/logo1.png';
 import './logo.styles.css'
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
-    <div className="logo-container">
+    <Link to='/'>
+      <div className="logo-container">
         <img className='steam-games-logo' src={logoImage} alt="logo" />
-    </div>
+      </div>
+    </Link>
   );
 }
 
